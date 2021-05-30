2316e081b453abfda9fa35f34852e205b0a8f696
Moksej <58531286+TheMoksej@users.noreply.github.com>
2021-05-30 21:06:13 +0200

    adding errors

--- exaroton.py/errors.py
+++ exaroton.py/errors.py
@@ -1,1 +1,18 @@
-# ignore
+class ExarotonPY(Exception):
+  pass
+
+class HTTPException(ExarotonPY):
+    """
+    Raised when accessing the API fails
+    """
+    def __init__(self, *args, **kwargs):
+        self.raised_error = kwargs.get('raised_error', None)
+        if isinstance(self.raised_error, dict):
+            self.message = self.raised_error.get('message', '')
+        else:
+            self.message = self.raised_error
+            self.status = None
+            self.status = kwargs.get('status', None)
+
+        err = f"Status code: {self.status} --> {self.message}"
+        super().__init__(err)
